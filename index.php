f6eef4b62184590808bfb7d0b2dcf2f19d7bd92b
moriarty3k <hdat1305@gmail.com>
2022-11-23 13:51:29 +0700

    reg success noti

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php 
 include('server.php');
-// session_start();	
-// $db = mysqli_connect('localhost', 'root', '', 'wokwokwokwok');
+//session_start();	
+
 
 if (!isset($_SESSION['username'])) {
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
 }
 if (isset($_GET['logout'])) {
-  	
+  	session_destroy();
   	unset($_SESSION['username']);
   	header("location: login.php");
 }
@@ -27,7 +27,17 @@ if (isset($_GET['logout'])) {
 	<h2>Home Page</h2>
 </div>
 <div class="content">
-  	 
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>	
+  	<?php endif ?> 
 
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
