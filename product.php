89c3fca6f21ba81835a2e4182c386444f02113db
moriarty3k <hdat1305@gmail.com>
2022-12-03 09:46:39 +0700

    haiz

--- product.php
+++ product.php
@@ -8,12 +8,12 @@ $result = mysqli_query($db,$query);
 
 
 if (isset($_POST["add"])) {
-    
-    if(isset($_SESSION["shopping_cart"])) {  
-        $_SESSION["success"] = "Item added!";
-        $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");  
-        if(!in_array($_GET["id"], $item_array_id)) {  
-              $count = count($_SESSION["shopping_cart"]);  
+    if(isset($_SESSION["shopping_cart"])) {     
+        $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");  //trả về giá trị item_id của array 
+        $item_array_quantity = array_column($_SESSION["shopping_cart"], "quantity");
+        $count = count($_SESSION["shopping_cart"]);
+        if(!in_array($_GET["id"], $item_array_id)) {  //tìm giá trị id từ get trong array $item_array_id
+              //$count = count($_SESSION["shopping_cart"]);  
               $item_array = array(  
                    'item_id'               =>     $_GET["id"],  
                    'item_name'               =>     $_POST["hidden_name"],  
@@ -21,14 +21,22 @@ if (isset($_POST["add"])) {
                    'item_quantity'          =>     $_POST["quantity"]  
               );  
               $_SESSION["shopping_cart"][$count] = $item_array;  
+              header('location:product.php');
               
          } 
-        //else {  
+        else {  
+            unset($_SESSION["shopping_cart"][$_GET['id']]);
+            //$count = count($_SESSION["shopping_cart"]);  
+            $item_array = array(  
+                 'item_id'               =>     $_GET["id"],  
+                 'item_name'               =>     $_POST["hidden_name"],  
+                 'item_price'          =>     $_POST["hidden_price"] ,  
+                 'item_quantity'          =>     $_SESSION["shopping_cart"][$_GET['id']]["item_quantity"] + $_POST["quantity"] 
+            );  
+            $_SESSION["shopping_cart"][$count] = $item_array;  
+            header('location:product.php');
             
-        //     // header('location: product.php');
-        //     $_SESSION["error"] = "Item already added!";
-        //     ;  
-        // }  
+        }  
     } else {  
         $item_array = array(  
               'item_id'               =>     $_GET["id"],  
@@ -37,6 +45,7 @@ if (isset($_POST["add"])) {
               'item_quantity'          =>     $_POST["quantity"]  
          );  
          $_SESSION["shopping_cart"][0] = $item_array;  
+         header('location:product.php');
     }  
 }  
 if(isset($_GET["action"]))  
@@ -48,10 +57,7 @@ if(isset($_GET["action"]))
             if($values["item_id"] == $_GET["id"])  
             {  
                 unset($_SESSION["shopping_cart"][$keys]); 
-                array_push($success, "Item removed");
-                header('location: product.php'); 
-                //    echo '<script>alert("Item Removed")</script>';  
-                //    echo '<script>window.location="product.php"</script>';  
+                header('location: product.php');  
             }  
          }  
     }
@@ -65,49 +71,13 @@ if(isset($_GET["action"]))
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Products</title>
-    <style>
-
-    .error {
-    width: 60%; 
-    margin: 0px auto; 
-    padding: 10px; 
-    border: 1px solid #a94442; 
-    color: #a94442; 
-    background: #f2dede; 
-    border-radius: 5px; 
-    text-align: center;
-  }
-
-    .success {
-    width: 60%; 
-    margin: 0px auto; 
-    padding: 10px; 
-    color: #3c763d; 
-    background: #dff0d8; 
-    border: 1px solid #3c763d; 
-    border-radius: 5px; 
-    text-align: center;
-  }
-    </style>
+  
     <link rel="stylesheet" type="text/css" href="/css/product.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 </head>
-<!--Changing the number in the column_# class changes the number of columns-->
 <body>
     <div id="wrap">
-        <!-- notification message -->
-        <?php if (isset($_SESSION['success'])) : ?>
-            <div class="success">
-                <h3>
-                <?php 
-                    echo $_SESSION['success']; 
-                    unset($_SESSION['success']);
-                ?>
-                </h3>
-            </div>	
-        <?php endif ?>
-        
-        
+      
         <div id="columns" class="columns_4">
         <?php  if (mysqli_num_rows($result) > 0) : ?> 
             <?php while($row = mysqli_fetch_assoc($result)) : ?>
@@ -117,7 +87,7 @@ if(isset($_GET["action"]))
                         <span class="id" name="id">ID: <?php echo $row['id'];?></span>
                         <h4 class="text-info"><?php echo $row["name"]; ?></h4>
                         <input type="text" name="quantity" class="form-control" value="1" />  
-                        <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
+                        <h4 class="text-danger"><?php echo $row["price"]; ?>.000 VND</h4>
                         <input class="button" type="submit" name="add" value="Add to cart">
                         <input type="hidden" name="hidden_name" value="<?php echo $row['name']?>">
                         <input type="hidden" name="hidden_price" value="<?php echo $row['price']?>">
@@ -149,8 +119,8 @@ if(isset($_GET["action"]))
             <tr>  
                 <td><?php echo $values["item_name"]; ?></td>  
                 <td><?php echo $values["item_quantity"]; ?></td>  
-                <td>$ <?php echo $values["item_price"]; ?></td>  
-                <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>  
+                <td><?php echo $values["item_price"]; ?>.000 VND</td>  
+                <td><?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?>0 VND</td>  
                 <td><a href="product.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>  
             </tr>  
             <?php  
